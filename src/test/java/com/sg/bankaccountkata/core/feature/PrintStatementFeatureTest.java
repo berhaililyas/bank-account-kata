880ef1793de062dc0893ea659d1fd00bd0adb49a
Ilyas BERHAIL <ilyas.berhail.ext@orange.com>
2021-12-07 12:00:50 +0100

    Print Statement Feature - print statement with multiple transactions

--- src/test/java/com/sg/bankaccountkata/core/feature/PrintStatementFeatureTest.java
+++ src/test/java/com/sg/bankaccountkata/core/feature/PrintStatementFeatureTest.java
@@ -12,7 +12,6 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
@@ -67,6 +66,25 @@ public class PrintStatementFeatureTest {
 
     @Test
     void shouldPrintStatementWithMultipleTransactions() {
-        fail("Not yet implemented");
+        // Input objects
+        Transaction deposit_1 = new Transaction(TransactionType.DEPOSIT, LocalDate.of(2021, 11, 28), 1000, 1000);
+        Transaction withdrawl = new Transaction(TransactionType.WITHDRAWL, LocalDate.of(2021, 11, 30), -100, 900);
+        Transaction deposit_2 = new Transaction(TransactionType.DEPOSIT, LocalDate.of(2021, 12, 05), 500, 1400);
+
+        // Expected objects
+        List<Transaction> transactions = Arrays.asList(deposit_1, withdrawl, deposit_2);
+
+        // Mockito expectations
+        given(transactionRepositoryOutputMock.findAllTransactions()).willReturn(transactions);
+
+        // Execute the method being tested
+        printStatementFeature.printStatement();
+
+        // Validation
+        verify(printerOutputMock).print(
+                "OPERATION | DATE | AMOUNT | BALANCE\n" +
+                        "DEPOSIT | 28/11/2021 | 1000,00€ | 1000,00€\n" +
+                        "WITHDRAWL | 30/11/2021 | -100,00€ | 900,00€\n" +
+                        "DEPOSIT | 05/12/2021 | 500,00€ | 1400,00€");
     }
 }
