6220767e02b4e99e3532d31ba5bc926c44bc40fb
Ilyas BERHAIL <ilyas.berhail.ext@orange.com>
2021-12-07 11:47:30 +0100

    Print Statement Feature - print statement with one transaction

--- src/main/java/com/sg/bankaccountkata/core/feature/PrintStatementFeature.java
+++ src/main/java/com/sg/bankaccountkata/core/feature/PrintStatementFeature.java
@@ -1,19 +1,51 @@
 package com.sg.bankaccountkata.core.feature;
 
+import com.sg.bankaccountkata.core.domain.Transaction;
 import com.sg.bankaccountkata.core.port.in.PrintStatementInputPort;
 import com.sg.bankaccountkata.core.port.out.PrinterOutput;
+import com.sg.bankaccountkata.core.port.out.TransactionRepositoryOutput;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.List;
 
 public class PrintStatementFeature implements PrintStatementInputPort {
 
     private static final String STATEMENT_HEADER = "OPERATION | DATE | AMOUNT | BALANCE";
+    private final static DateTimeFormatter DATE_FORMAT =
+            new DateTimeFormatterBuilder().appendPattern("dd/MM/yyyy").toFormatter();
 
     private final PrinterOutput printerOutput;
+    private final TransactionRepositoryOutput transactionRepositoryOutput;
 
-    public PrintStatementFeature(PrinterOutput printerOutput) {
+
+    public PrintStatementFeature(PrinterOutput printerOutput,
+                                 TransactionRepositoryOutput transactionRepositoryOutput) {
         this.printerOutput = printerOutput;
+        this.transactionRepositoryOutput = transactionRepositoryOutput;
     }
 
     public void printStatement() {
-        printerOutput.print(STATEMENT_HEADER);
+        List<Transaction> transactions = this.transactionRepositoryOutput.findAllTransactions();
+
+        StringBuilder statement = new StringBuilder();
+
+        statement.append(STATEMENT_HEADER);
+
+        transactions.stream()
+                .forEach(transaction -> {
+                    statement.append("\n");
+                    statement.append(format(transaction));
+                });
+
+        printerOutput.print(statement.toString());
+    }
+
+    private String format(Transaction transaction) {
+        return String.format("%s" + " | " + "%s" + " | " + "%.2f€" + " | " + "%.2f€",
+                transaction.getTransactionType().toString(),
+                transaction.getDate().format(DATE_FORMAT),
+                (float) transaction.getAmount(),
+                (float) transaction.getBalance());
     }
 }
--- src/test/java/com/sg/bankaccountkata/core/feature/PrintStatementFeatureTest.java
+++ src/test/java/com/sg/bankaccountkata/core/feature/PrintStatementFeatureTest.java
@@ -1,12 +1,20 @@
 package com.sg.bankaccountkata.core.feature;
 
+import com.sg.bankaccountkata.core.domain.Transaction;
+import com.sg.bankaccountkata.core.domain.TransactionType;
 import com.sg.bankaccountkata.core.port.out.PrinterOutput;
+import com.sg.bankaccountkata.core.port.out.TransactionRepositoryOutput;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
@@ -15,12 +23,14 @@ public class PrintStatementFeatureTest {
     private PrintStatementFeature printStatementFeature;
 
     private PrinterOutput printerOutputMock;
+    private TransactionRepositoryOutput transactionRepositoryOutputMock;
 
     @BeforeEach
     void setUp() {
         this.printerOutputMock = Mockito.mock(PrinterOutput.class);
+        this.transactionRepositoryOutputMock = Mockito.mock(TransactionRepositoryOutput.class);
 
-        this.printStatementFeature = new PrintStatementFeature(printerOutputMock);
+        this.printStatementFeature = new PrintStatementFeature(printerOutputMock, transactionRepositoryOutputMock);
     }
 
     @Test
@@ -37,7 +47,22 @@ public class PrintStatementFeatureTest {
 
     @Test
     void shouldPrintStatementWithOneTransaction() {
-        fail("Not yet implemented");
+        // Input objects
+        Transaction transaction = new Transaction(TransactionType.DEPOSIT, LocalDate.of(2021, 11, 28), 1000, 1000);
+
+        // Expected objects
+        List<Transaction> transactions = Arrays.asList(transaction);
+
+        // Mockito expectations
+        given(transactionRepositoryOutputMock.findAllTransactions()).willReturn(transactions);
+
+        // Execute the method being tested
+        printStatementFeature.printStatement();
+
+        // Validation
+        verify(printerOutputMock).print(
+                "OPERATION | DATE | AMOUNT | BALANCE\n" +
+                        "DEPOSIT | 28/11/2021 | 1000,00€ | 1000,00€");
     }
 
     @Test
